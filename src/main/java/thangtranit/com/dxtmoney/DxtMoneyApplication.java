166b04f5df23fba11387b9e1ed012b4165d177da
thangtran0111 <xuanthang0111@gmail.com>
2024-05-09 23:25:57 +0700

    INIT: start project

---
+++ src/main/java/thangtranit/com/dxtmoney/DxtMoneyApplication.java
@@ -0,0 +1,13 @@
+package thangtranit.com.dxtmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxtMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxtMoneyApplication.class, args);
+    }
+
+}
